8197375da33485f330d5d7c26a62600bbe697545
dghousi <dawlatzai.ghousi@gmail.com>
2025-02-06 07:12:08 +0430

    Add a custom hook for preVious count and text

--- src/components/Effects/EffectApp.jsx
+++ src/components/Effects/EffectApp.jsx
@@ -3,26 +3,28 @@ import { useEffect, useRef, useState } from "react"
 function EffectApp() {
     const [count, setCount] = useState(0)
     const [text, setText] = useState('')
-
-    const prevCount = useRef()
-    useEffect(() => {
-        prevCount.current = count
-    }, [count])
-
-    const prevText = useRef()
-    useEffect(() => {
-        prevText.current = text
-    }, [text])
+    const prevCount = usePrevious(count)
+    const prevText = usePrevious(text)
 
     return (
         <>
             <button onClick={() => setCount(count +1)}>Increment</button>
             <p>Count: {count}</p>
-            <p>Previous render count: {prevCount.current}</p>
+            <p>Previous render count: {prevCount}</p>
             <input value={text} onChange={(event) => setText(event.target.value)} />
-            <p>Previous render text: {prevText.current}</p>
+            <p>Previous render text: {prevText}</p>
         </>
     )
+}
+
+function usePrevious(value) {
+    const prevRef = useRef()
+    useEffect(() => {
+        prevRef.current = value
+    }, [value])
+
+    console.log(prevRef, 'PreRefValue')
+    return prevRef.current
 }
 
 export default EffectApp
